b849a299348eb4217d5c1abc31503ec1e04e6423
oh3vci <oh3vci@gmail.com>
2018-05-07 17:34:30 +0900

    ENH: Block generation & destroy

---
+++ Assets/Scripts/BlockController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject.name == "Failcollider")
+		{
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	// up => 1
+	// down => 2
+	// left => 3
+	// right => 4
+	// turn => 5
+	// tap = 6
+	List<float> whichBlock = new List<float>() {1, 2, 1, 1, 3, 4, 5, 6, 6, 4};
+
+	public int blockMark = 0;
+	public Transform blockObj;
+	public string timerReset = "y";
+	public string blockType;
+	public float xPos;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (timerReset == "y")
+		{
+			StartCoroutine (spawnBlock ());
+			timerReset = "n";
+		}
+	}
+
+	IEnumerator spawnBlock ()
+	{
+		yield return new WaitForSeconds (1);
+
+		if (whichBlock [blockMark] == 1)
+		{
+			blockType = "up";
+		}
+		if (whichBlock [blockMark] == 2)
+		{
+			blockType = "down";
+		}
+		if (whichBlock [blockMark] == 3)
+		{
+			blockType = "left";
+		}
+		if (whichBlock [blockMark] == 4)
+		{
+			blockType = "right";
+		}
+		if (whichBlock [blockMark] == 5)
+		{
+			blockType = "turn";
+		}
+		if (whichBlock [blockMark] == 6)
+		{
+			blockType = "tap";
+		}
+
+		blockMark += 1;
+		timerReset = "y";
+		Instantiate (blockObj, new Vector3(0.05f, 1.8f, -3.2f), blockObj.rotation);
+	}
+}
